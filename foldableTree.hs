927c9de07f30f578ae5151ea52a7d768019aff0e
Mich Ochieng <michochieng@gmail.com>
2022-03-23 12:46:06 -0700

    Removed test

--- foldableTree.hs
+++ foldableTree.hs
@@ -22,18 +22,7 @@ treeTestA = foldl (\acc x -> x * acc) 1
 treeTestB :: Tree Int -> Int
 treeTestB = foldl (\acc x -> x + acc) 0
 
-testCheckA :: Int -> String 
-testCheckA x = 
-    if x == 5040
-        then "Test A successfully produced 5040."
-        else "Error occured in test A."
-
-testCheckB :: Int -> String
-testCheckB x = 
-    if x == 28
-        then "Test B successfully produced 28"
-        else "Error occured in test B."
 main = do
-    print . testCheckA $ treeTestA myTree
-    print . testCheckB $ treeTestB myTree
+    print $ treeTestA myTree
+    print $ treeTestB myTree
     
