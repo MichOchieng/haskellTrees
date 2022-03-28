f86b6e0627d62af7a16e68ce003a9c6eccc57241
Mich Ochieng <michochieng@gmail.com>
2022-03-27 20:49:53 -0700

    Additions

--- pointFree.hs
+++ pointFree.hs
@@ -1,4 +1,15 @@
 module Main where
 
+
+test :: (Monad m) => m a -> m a
+test = \ mm -> do {
+        m <- mm ; return m
+    }
+
+-- test mm = do {
+--         m <- mm ; m
+--     }
+
 main = do
+    print $ test [1]
     print "hi"
