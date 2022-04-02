f005ac01891ebddcbefc98cd9ab370955ed7f48d
Mich Ochieng <michochieng@gmail.com>
2022-04-02 01:36:20 -0700

    Added examples

--- pointFree.hs
+++ pointFree.hs
@@ -1,15 +1,27 @@
-module Main where
+--  Question 15
 
-
-test :: (Monad m) => m a -> m a
-test = \ mm -> do {
-        m <- mm ; return m
+expr :: (Monad m) => m (m a) -> m a
+expr = \ mm -> do {
+        m <- mm ; m
     }
 
--- test mm = do {
+-- Expanded
+-- (1) \mm -> do {
 --         m <- mm ; m
 --     }
+-- (2) \mm -> (mm >>= (\m -> do m))
+--     (\m -> do m) can be replaced with the identity function since its just outputting the input
+-- (3) \mm -> (mm >>= id)
 
-main = do
-    print $ test [1]
-    print "hi"
+-- Question 16
+
+myUncurry :: (a -> b -> c) -> (a,b) -> c
+myUncurry f (a,b) = f a b
+
+-- This will take a multi-argument function and return the same function with one argument, 
+-- along with the remaining arguments
+
+someFunc :: Int -> Int -> Int 
+someFunc a b = a + b
+
+    
